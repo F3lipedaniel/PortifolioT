bfc838de669575a7076ac4b9dfb1e90c57f1c0aa
Felipe Daniel <f3lipedaniel@gmail.com>
2022-09-09 22:29:30 -0300

    components

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -3,7 +3,24 @@ import styles from './link.module.css'
 export function Link () {
     return (
         <aside className={styles.link}>
-           
+            <div className={styles.pin}>
+                <img className={styles.mapPin} src='src\assets\map-pin.svg'></img>
+                <img className={styles.briefcasePin} src='src\assets\briefcase.svg'></img>
+                <img className={styles.githubPin} src='src\assets\github.svg'></img>
+                <img className={styles.linkedinPin} src='src\assets\linkedin.svg'></img>
+                <img className={styles.twitterPin} src='src\assets\twitter.svg'></img>
+                <img className={styles.globePin} src='src\assets\globe.svg'></img>
+                <img className={styles.mailPin} src='src\assets\mail.svg'></img>
+            </div>
+            <div>
+                <span className={styles.brasilText}>Brasil</span>
+                <span className={styles.briefcaseText}>Delivery Direto</span>
+                <span className={styles.githubText}>F3lipedaniel</span>
+                <span className={styles.linkedinText}>F3lipedaniel</span>
+                <span className={styles.twitterText}>F3lipedaniel</span>
+                <span className={styles.siteText}>-</span>
+                <span className={styles.emailText} src=''>F3lipedaniel@gmail.com</span>
+            </div>
         </aside>
     )
 }
